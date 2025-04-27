30500f403456385939bdea0808bde88c527dce39
Manuel Guerrero <manuelguerrero@paucasesnovescifp.cat>
2025-04-27 16:43:48 +0200

    Añadido modo eliminar

--- assets/js/getAll.js
+++ assets/js/getAll.js
@@ -9,6 +9,7 @@ export async function getAll(){
     let titulo = params.get("titulo");
     let tipo = params.get("tipo");
     let generos = params.getAll("genero");
+    let isEliminar = params.get("eliminar");
 
     let extras = [];
     if (titulo)
@@ -23,27 +24,69 @@ export async function getAll(){
     
 
     let queryParams = extras.length > 0 ? "?" + extras.join("&") : "";
-    console.log(queryParams);
 
     let resultados = await apiGet(`http://localhost:8000/get-all/${currentPage}${queryParams}`);
     let total_paginas = resultados.total_paginas;
     renderPagination(currentPage, total_paginas, cards);
-    let a,img,h3,p,div,span;
+    let principal,img,h3,p,div,span;
     resultados.peliculas.forEach(elemento => {
-        a = document.createElement("a");
+        let selector = null;
+        if (isEliminar){
+            console.log(elemento)
+            if (elemento.type == "Serie"){
+                selector = document.createElement("select");
+                selector.classList.add("mb-2", "p-2", "lg:mb-0", "bg-deep-black", "text-center", "text-gray-blue", "rounded-md");
+                // Creamos el selector base
+                let opcion = document.createElement("option");
+                opcion.value = '';
+                opcion.textContent = '-- Selecciona un capítulo --';
+                opcion.selected = true;
+                opcion.disabled = true;
+                selector.appendChild(opcion);
+                // Opcion todos
+                opcion = document.createElement("option");
+                opcion.value = "TODOS";
+                opcion.textContent = "TODOS";
+                selector.appendChild(opcion);
+                elemento.capitulos.forEach(capitulo => {
+                    opcion = document.createElement("option");
+                    opcion.value = capitulo;
+                    opcion.textContent = capitulo;
+                    selector.appendChild(opcion);
+                });
+            }
+            principal = document.createElement("div");
+            principal.id = elemento.id;
+            principal.classList.add("cursor-pointer");
+            principal.addEventListener('click', (event) => {
+                let selected = cards.querySelector('.selected');
+                if (selected)
+                    selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-offset-2', 'selected');
+                else{
+                    let eliminarButton = document.getElementById("eliminar");
+                    eliminarButton.disabled = false;
+                }
+                // Seleccionar esta
+                event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-offset-2', 'selected');
+            });
+        } else {
+            principal = document.createElement("a");
+            principal.href = `watch.html?id=${elemento.id}`
+        }
         img = document.createElement("img");
         h3 = document.createElement("h3");
         p = document.createElement("p");
         div = document.createElement("div");
         
-        cards.appendChild(a);
-        a.appendChild(img);
-        a.appendChild(h3);
-        a.appendChild(p);
-        a.appendChild(div);
+        cards.appendChild(principal);
+        principal.appendChild(img);
+        principal.appendChild(h3);
+        principal.appendChild(p);
+        principal.appendChild(div);
+        if (selector)
+            principal.appendChild(selector);
         
-        a.classList.add("movie-card", "bg-steel-blue", "p-4", "rounded-lg", "shadow-lg", "flex", "flex-col", "items-center", "hover:scale-105", "transition");
-        a.href = `watch.html?id=${elemento.id}`
+        principal.classList.add("movie-card", "bg-steel-blue", "p-4", "rounded-lg", "shadow-lg", "flex", "flex-col", "items-center", "hover:scale-105", "transition");
         img.src = !elemento.poster ? "../assets/img/poster.jpg" : elemento.poster;
         img.alt = "Portada";
         img.classList.add("w-full", "rounded-md", "shadow-md");
@@ -51,7 +94,7 @@ export async function getAll(){
         h3.innerText = elemento.title;
         p.classList.add("movie-type", "text-sm", "text-gray-blue", "text-center", "bg-neon-cyan/20", "px-2", "py-1", "rounded-md");
         p.innerText = elemento.type;
-        div.classList.add("flex", "flex-wrap", "gap-2", "mt-2");
+        div.classList.add("flex", "flex-wrap", "gap-2", "my-2");
         elemento.genres.forEach(genero => {
             span = document.createElement("span");
             
