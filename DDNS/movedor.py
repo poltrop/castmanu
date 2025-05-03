4f8cdde9540b7f2a86b8648e04c210c95b035e1f
Manuel Guerrero <manuelguerrero@paucasesnovescifp.cat>
2025-05-03 19:52:47 +0200

    Añadida posibilidad de que no se mueva si ya existe en la otra carpeta y que si deja de existir de golpe salga del bucle y no espere el timeout

--- DDNS/movedor.py
+++ DDNS/movedor.py
@@ -10,6 +10,9 @@ def wait_for_file(file_path, timeout=600, check_interval=5, stable_checks=3):
     stable_count = 0
 
     while timeout > 0:
+        if not os.path.exists(file_path):
+            print(f"❌ El archivo {file_path} ha desaparecido.")
+            return False  # Abortamos
         try:
             size = os.path.getsize(file_path)
             if size == prev_size:
@@ -35,9 +38,11 @@ while True:
         if os.path.isfile(src_path):
             print(f"Detectado: {src_path} – comprobando estabilidad...")
             if wait_for_file(src_path):
-                print(f"✅ Estable. Moviendo {src_path} → {dst_path}")
                 if not os.path.exists(dst_path):
                     shutil.move(src_path, dst_path)
+                    print(f"✅ Estable. Moviendo {src_path} → {dst_path}")
+                else:
+                    print(f"No se ha movido porque ya existe")
             else:
                 print(f"⚠️ El archivo {src_path} no se estabilizó a tiempo.")
     time.sleep(2)
