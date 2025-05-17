70350d1fa62586154a4569436f0225fe62b51075
Manuel Guerrero <manuelguerrero@paucasesnovescifp.cat>
2025-05-17 19:46:39 +0200

    Modificada funcion para funcionar por carruseles

--- assets/js/getAll.js
+++ assets/js/getAll.js
@@ -1,11 +1,9 @@
 import { apiGet } from "./api.js";
 import { mapGeneroInverso } from "./mapGeneros.js";
-import { renderPagination } from "./paginacion.js";
 
 export async function getAll(){
     let cards = document.getElementById("cards");
     let params = new URLSearchParams(window.location.search);
-    let currentPage = parseInt(params.get("pagina")) || 1;
     let titulo = params.get("titulo");
     let tipo = params.get("tipo");
     let generos = params.getAll("genero");
@@ -26,14 +24,22 @@ export async function getAll(){
 
     let queryParams = extras.length > 0 ? "?" + extras.join("&") : "";
 
-    let resultados = await apiGet(`http://localhost:8000/get-all/${currentPage}${queryParams}`);
-    let total_paginas = resultados.total_paginas;
-    renderPagination(currentPage, total_paginas, cards);
+    let resultados = await apiGet(`http://localhost:8000/get-all${queryParams}`);
+    let generosUnicos = [...new Set(resultados.peliculas.flatMap(pelicula => pelicula.genres))];
+    
+    generosUnicos.forEach(genero => {
+        CreateGenreCarrusel(genero);
+    });
+    
+    if (resultados.peliculas.some(pelicula => pelicula.genres.length == 0)) {
+        CreateGenreCarrusel("Sin Género");
+        generosUnicos.push("Sin Género");
+    } 
+    
     let principal,img,h3,p,div,span;
     resultados.peliculas.forEach(elemento => {
         let selector = null;
         if (isEliminar){
-            console.log(elemento)
             if (elemento.type == "Serie"){
                 selector = document.createElement("select");
                 selector.classList.add("mb-2", "p-2", "lg:mb-0", "bg-deep-black", "text-center", "text-gray-blue", "rounded-md");
@@ -43,6 +49,7 @@ export async function getAll(){
                 opcion.textContent = '-- Selecciona un capítulo --';
                 opcion.selected = true;
                 opcion.disabled = true;
+                console.log(opcion)
                 selector.appendChild(opcion);
                 // Opcion todos
                 opcion = document.createElement("option");
@@ -62,13 +69,13 @@ export async function getAll(){
             principal.addEventListener('click', (event) => {
                 let selected = cards.querySelector('.selected');
                 if (selected)
-                    selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-offset-2', 'selected');
+                    selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
                 else{
                     let eliminarButton = document.getElementById("eliminar");
                     eliminarButton.disabled = false;
                 }
                 // Seleccionar esta
-                event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-offset-2', 'selected');
+                event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
             });
         } else if (isEditar) {
             principal = document.createElement("div");
@@ -77,13 +84,13 @@ export async function getAll(){
             principal.addEventListener('click', (event) => {
                 let selected = cards.querySelector('.selected');
                 if (selected)
-                    selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-offset-2', 'selected');
+                    selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
                 else{
                     let editarButton = document.getElementById("editar");
                     editarButton.disabled = false;
                 }
                 // Seleccionar esta
-                event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-offset-2', 'selected');
+                event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
             });
         } else {
             principal = document.createElement("a");
@@ -94,31 +101,139 @@ export async function getAll(){
         p = document.createElement("p");
         div = document.createElement("div");
         
-        cards.appendChild(principal);
         principal.appendChild(img);
         principal.appendChild(h3);
         principal.appendChild(p);
         principal.appendChild(div);
         if (selector)
             principal.appendChild(selector);
+
+        principal.querySelector("div").addEventListener('click', (event) => {
+            event.stopPropagation();
+            event.preventDefault();
+            let genero = event.target.closest('span').innerText;
+
+            if (generos.includes(genero)) params.delete("genero", genero);
+            else params.append("genero", genero);
+            
+            let queryParams = params.toString();
+            window.location.href = `home.html${queryParams ? '?' + queryParams : ''}`;
+        });
+
+        principal.querySelector("p").addEventListener('click', (event) => {
+            event.stopPropagation();
+            event.preventDefault();
+            let type = event.target.closest('p').innerText;
+
+            if (tipo == type) params.delete("tipo");
+            else params.set("tipo", type);
+            
+            let queryParams = params.toString();
+            window.location.href = `home.html${queryParams ? '?' + queryParams : ''}`;
+        });
         
-        principal.classList.add("movie-card", "bg-steel-blue", "p-4", "rounded-lg", "shadow-lg", "flex", "flex-col", "items-center", "hover:scale-105", "transition");
+        principal.classList.add("movie-card","bg-steel-blue","w-[440px]","p-4","flex-shrink-0","rounded-lg","shadow-lg","flex","flex-col","items-center","hover:brightness-110","hover:saturate-125","transition");
         img.src = !elemento.poster ? "../assets/img/poster.jpg" : elemento.poster;
         img.alt = "Portada";
-        img.classList.add("w-full", "rounded-md", "shadow-md");
+        img.className = "w-full h-[600px] rounded-md shadow-md object-cover";
         h3.classList.add("text-neon-cyan", "text-lg", "font-bold", "mt-2", "text-center");
         h3.innerText = elemento.title;
-        p.classList.add("movie-type", "text-sm", "text-gray-blue", "text-center", "bg-neon-cyan/20", "px-2", "py-1", "rounded-md");
+        p.classList.add("movie-type", "text-sm", "text-gray-blue", "text-center", "bg-neon-cyan/20", "px-2", "py-1", "rounded-md", "cursor-pointer");
         p.innerText = elemento.type;
+        if (tipo == elemento.type) p.classList.add('ring-2', 'ring-white');
         div.classList.add("flex", "flex-wrap", "gap-2", "my-2");
+
         elemento.genres.forEach(genero => {
             span = document.createElement("span");
             
             div.appendChild(span);
             
-            span.classList.add("movie-genre", "bg-neon-cyan", "text-midnight-blue", "px-3", "py-1", "rounded-full", "text-xs", "font-bold");
+            span.classList.add("movie-genre", "bg-neon-cyan", "text-midnight-blue", "px-3", "py-1", "rounded-full", "text-xs", "font-bold", "cursor-pointer");
+            if (generos.includes(genero)) span.classList.add('ring-2', 'ring-white');
             span.setAttribute("genero",genero);
             span.innerText = genero;
         });
+        
+        let clone;
+        elemento.genres.forEach(genero => {
+            clone = principal.cloneNode(true);
+            clone.querySelector("div").addEventListener('click', (event) => {
+                event.stopPropagation();
+                event.preventDefault();
+                let genero = event.target.closest('span').innerText;
+
+                if (generos.includes(genero)) params.delete("genero", genero);
+                else params.append("genero", genero);
+
+                let queryParams = params.toString();
+                window.location.href = `home.html${queryParams ? '?' + queryParams : ''}`;
+            });
+
+            
+            clone.querySelector("p").addEventListener('click', (event) => {
+                event.stopPropagation();
+                event.preventDefault();
+                let type = event.target.closest('p').innerText;
+                
+                if (tipo == type) params.delete("tipo");
+                else params.set("tipo", type);
+                
+                let queryParams = params.toString();
+                window.location.href = `home.html${queryParams ? '?' + queryParams : ''}`;
+            });
+
+            if (isEliminar) {
+                clone.addEventListener('click', (event) => {
+                    let selected = cards.querySelector('.selected');
+                    if (selected)
+                        selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
+                    else{
+                        let eliminarButton = document.getElementById("eliminar");
+                        eliminarButton.disabled = false;
+                    }
+                    // Seleccionar esta
+                    event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
+                });
+
+                if (clone.querySelector("p").innerText == "Serie") clone.querySelector("select").value = "";
+            }
+
+            if (isEditar) {
+                clone.addEventListener('click', (event) => {
+                    let selected = cards.querySelector('.selected');
+                    if (selected)
+                        selected.classList.remove('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
+                    else{
+                        let editarButton = document.getElementById("editar");
+                        editarButton.disabled = false;
+                    }
+                    // Seleccionar esta
+                    event.target.closest('.movie-card').classList.add('ring-4', 'ring-neon-cyan', 'ring-inset', 'selected');
+                });
+            }
+
+            document.getElementById(`genero-${genero}`).querySelector("div").appendChild(clone);
+        });
+
+        if (elemento.genres.length == 0) {
+            document.getElementById(`genero-Sin Género`).querySelector("div").appendChild(principal);
+        }
     });
+    return generosUnicos;
+}
+
+function CreateGenreCarrusel (genero) {
+    let divGenero = document.createElement("div");
+    let h3 = document.createElement("h3");
+    let div = document.createElement("div");
+
+    cards.appendChild(divGenero);
+    divGenero.appendChild(h3);
+    divGenero.appendChild(div);
+
+    divGenero.id = `genero-${genero}`;
+    divGenero.className = "w-full";
+    div.className = "flex gap-4 overflow-x-auto overflow-y-hidden";
+    h3.innerText = genero;
+    h3.className = "text-lg sm:text-xl text-neon-cyan font-bold mb-6";
 }
